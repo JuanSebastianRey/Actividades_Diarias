0f35fe8fc7c3d52c2b892e7b2ef37f7671736fbf
JuanSebastianRey <sebastianreyes25@outlook.es>
2024-10-23 15:39:34 -0500

    feat: :sparkles: Endpoint getUserById finished

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,22 @@
 const { User } = require("../model/users.model");
 
+
 exports.getUsers = async (req, res) => {
     try {
         const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.getUsersById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const users = await User.findById(id);
+        if (!users) {
+            return res.status(404).json({ message: "User not found" });
+        }
         res.json(users);
     } catch (error) {
         res.status(500).json({ message: error.message });
--- server/routes/user.mongo.routes.js
+++ server/routes/user.mongo.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {getUsers} = require('../controllers/userController');
+const {getUsers, getUsersById} = require('../controllers/userController');
 
 router.get('/usuarios', getUsers)
+router.get('/usuarios/:id', getUsersById)
 
 module.exports = router;
