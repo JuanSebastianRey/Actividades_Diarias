e0e2d3c38c8a35cd6b5e61e22c63e4280a1dead3
JuanSebastianRey <sebastianreyes25@outlook.es>
2024-10-23 16:05:50 -0500

    feat: :sparkles: Endpoint getActivitiesById finished

--- server/controllers/activityController.js
+++ server/controllers/activityController.js
@@ -1,11 +1,21 @@
 const { Activity } = require("../model/activity.model");
 
-
 exports.getActivities = async (req, res) => {
-    try {
-        const activities = await Activity.find({},'-_id');
-        res.json(activities);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const activities = await Activity.find({}, "-_id");
+    res.json(activities);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getActivitiesById = async (req, res) => {
+  try {
+    const activity = await Activity.findById(req.params.id, '-_id');
+    if (!activity)
+      return res.status(404).json({ message: "No se encontró la actividad" });
+    res.json(activity);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/user.mongo.routes.js
+++ server/routes/user.mongo.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {getUsers, getUsersById} = require('../controllers/userController');
-const { getActivities } = require('../controllers/activityController');
+const { getActivities, getActivitiesById } = require('../controllers/activityController');
 
 router.get('/usuarios', getUsers)
 router.get('/usuarios/:id', getUsersById)
 router.get('/actividades', getActivities)
+router.get('/actividades/:id', getActivitiesById)
 
 module.exports = router;
