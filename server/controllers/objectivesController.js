0f48ff06d9223a712636dd890dd80a5fae608b4f
JuanSebastianRey <sebastianreyes25@outlook.es>
2024-10-23 16:21:12 -0500

    feat: :sparkles: Endpoint getObjectiveById finished

--- server/controllers/objectivesController.js
+++ server/controllers/objectivesController.js
@@ -9,3 +9,14 @@ exports.getObjective = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+exports.getObjectiveById = async (req, res) => {
+    try {
+        const objective = await Objective.findById(req.params.id, '-_id');
+        if (!objective)
+            return res.status(404).json({ message: "No se encontró el objetivo" });
+        res.json(objective);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- server/routes/user.mongo.routes.js
+++ server/routes/user.mongo.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {getUsers, getUsersById} = require('../controllers/userController');
 const { getActivities, getActivitiesById } = require('../controllers/activityController');
 const { getReminders, getRemindersById } = require('../controllers/rememberController');
-const { getObjective } = require('../controllers/objectivesController');
+const { getObjective, getObjectiveById } = require('../controllers/objectivesController');
 
 
 router.get('/usuarios', getUsers)
@@ -13,5 +13,6 @@ router.get('/actividades/:id', getActivitiesById)
 router.get('/recordatorios', getReminders)
 router.get('/recordatorios/:id', getRemindersById)
 router.get('/objetivos', getObjective)
+router.get('/objetivos/:id', getObjectiveById)
 
 module.exports = router;
